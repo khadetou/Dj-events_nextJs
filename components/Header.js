152cc36dd8c29cbe5ca66c086126f1a4e924b173
khadetou <khadetou96@gmail.com>
2021-08-01 13:34:16 +0000

    persist logged in user

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
                 </Link>
               </li>
               <li>
-                <Link href="/events/dashboard">
+                <Link href="/account/dashboard">
                   <a>Dashboard</a>
                 </Link>
               </li>
---
+++ pages/api/user.js
@@ -0,0 +1,33 @@
+import { API_URL } from "@/config/index";
+import cookie from "cookie";
+
+export default async (req, res) => {
+  if (req.method === "GET") {
+    const cookies = cookie.parse(req.headers.cookie);
+
+    if (!cookies) {
+      res.status(403).json({ message: "Not Authorized" });
+      return;
+    }
+
+    const { token } = cookies;
+
+    const strapiRes = await fetch(`${API_URL}/users/me`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    const user = await strapiRes.json();
+
+    if (strapiRes.ok) {
+      res.status(200).json({ user });
+    } else {
+      res.status(403).json({ message: "Message forbidden" });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+};
